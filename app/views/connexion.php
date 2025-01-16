48a1dca907cfad810ef54818eec09f6721935f3d
404diaby <diabymamadou3001@gmail.com>
2025-01-16 19:04:15 +0100

    cm

--- app/views/connexion.php
+++ app/views/connexion.php
@@ -8,7 +8,7 @@
     <title>Connexion</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body class="">
+<body class="h-10">
 
 <div class="container">
     <h1>Connectez-vous ou créez votre compte <!-- TODO: notre nom de site--> </h1>
